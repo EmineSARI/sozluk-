168c34b36b1810e5253576f856c1d7d66f7a7453
EmineSARI <39420397+EmineSARI@users.noreply.github.com>
2021-09-02 14:12:51 +0300

    Proje dosyası ekle.

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,32 @@
+﻿using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+   public class CategoryManager
+    {
+        GenericRepositorycs<Category> repo = new GenericRepositorycs<Category>();
+
+        public List<Category> GetAll()
+        {
+            return repo.List();
+        }
+        public void CategoryAddBL(Category p)
+        {
+            if(p.CategotyName == "" || p.CategotyName.Length <= 3 || 
+                p.CategotyDescription =="" || p.CategotyName.Length >= 51)
+            {
+                //hata mesajı
+            }
+            else
+            {
+                repo.Insert(p);
+            }
+        }
+    }
+}
