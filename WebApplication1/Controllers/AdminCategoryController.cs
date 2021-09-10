5abfd711cedcfdacb5ce58667939d6e2cf6606cc
EmineSARI <39420397+EmineSARI@users.noreply.github.com>
2021-09-10 17:26:45 +0300

    Edit

--- WebApplication1/Controllers/AdminCategoryController.cs
+++ WebApplication1/Controllers/AdminCategoryController.cs
@@ -65,5 +65,13 @@ namespace WebApplication1.Controllers
             var categoryvalue = cm.GetByID(id);
             return View(categoryvalue);
         }
+        [HttpPost]
+        public ActionResult EditCategory(Category p)
+        {
+            cm.CategoryUpdate(p);
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
