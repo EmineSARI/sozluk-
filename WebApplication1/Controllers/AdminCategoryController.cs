bf416b66457dce274ef4768bca6885f1adfc5835
EmineSARI <39420397+EmineSARI@users.noreply.github.com>
2021-09-10 16:17:55 +0300

    son güncelleme

--- WebApplication1/Controllers/AdminCategoryController.cs
+++ WebApplication1/Controllers/AdminCategoryController.cs
@@ -1,5 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +18,39 @@ namespace WebApplication1.Controllers
         {
             var categoryvalues = cm.GetList();
             return View(categoryvalues);
+        }
+
+
+        [HttpGet]
+
+        public ActionResult AddCategory()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+
+        public ActionResult AddCategory(Category p)
+        {
+            CategoryValidator categoryValidator = new CategoryValidator();
+
+            ValidationResult result = categoryValidator.Validate(p);
+            if (result.IsValid)
+            {
+                cm.ICategoryAdd(p);
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+
         }
     }
 }
