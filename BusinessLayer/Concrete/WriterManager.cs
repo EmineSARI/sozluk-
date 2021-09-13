40a8aa4868e973b39ac21bf42009e717f3c95d6c
EmineSARI <39420397+EmineSARI@users.noreply.github.com>
2021-09-13 11:47:46 +0300

    Yazar İşlemleri

---
+++ BusinessLayer/Concrete/WriterManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class WriterManager : IWriterService
+    {
+        IWriterDal _writeDal;
+
+        public WriterManager(IWriterDal writeDal)
+        {
+            _writeDal = writeDal;
+        }
+
+        public Writer GetByID(int id)
+        {
+            return _writeDal.Get(x=>x.WriterID==id);
+        }
+
+        public List<Writer> GetList()
+        {
+            return _writeDal.List();
+        }
+
+        public void WriterAdd(Writer writer)
+        {
+            _writeDal.Insert(writer);
+        }
+
+        public void WriterDelete(Writer writer)
+        {
+            _writeDal.Delete(writer);
+        }
+
+        public void WriterUpdate(Writer writer)
+        {
+            _writeDal.Update(writer);
+        }
+    }
+}
---
+++ WebApplication1/Controllers/WriterController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class WriterController : Controller
+    {
+        WriterManager wm = new WriterManager(new EFWriterDal());
+
+        public ActionResult Index()
+        {
+            var WriterValues = wm.GetList();
+            return View(WriterValues);
+        }
+    }
+}
