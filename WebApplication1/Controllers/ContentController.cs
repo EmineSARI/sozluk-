4c166c88cbbbe5442a292c4c704a092421a2b567
EmineSARI <39420397+EmineSARI@users.noreply.github.com>
2021-09-21 14:11:12 +0300

    Başlıklara Göre İçeriklerin Yüklenmesi

---
+++ WebApplication1/Controllers/ContentController.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ContentController : Controller
+    {
+        // GET: Content
+
+        ContentManager cm = new ContentManager(new EFContentDal());
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ContentByHeading( int id )
+        {
+            var contentvalues = cm.GetListByID(id);
+            return View();
+        }
+
+    }
+}
